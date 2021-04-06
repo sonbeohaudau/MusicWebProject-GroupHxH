eb42571fe10c30724d22f07938243c12f2201526
kurokage99 <hung.tp176774@sis.hust.edu.vn>
2021-04-06 23:05:30 +0700

    initiate MVC structure

---
+++ site/Model/M_Song.php
@@ -0,0 +1,34 @@
+<?php
+include_once('E_Song.php');
+include_once('connection.php');
+
+class Model_Song{
+	function __construct(){}
+	
+	function getAllSong(){
+		$db = DB::getInstance();
+		$stmt = $db->prepare('Select * from Song');
+		$result = $stmt->execute();
+		$songList = array();
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+			
+			array_push($songList, new Entity_Song($row['songID'],$row['rating'],$row['country']));
+		}
+		return $songList;
+	}
+	
+	function getSongByID($songID){
+		$db = DB::getInstance();
+		$stmt = $db->prepare('Select * from Song where songID = ?');
+		$result = $stmt->execute($songID);
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+    	{
+        	echo $row['country']."-".$row['proposer']."-".$row['rating']."<br>";
+    	}
+	}
+	
+}
+
+//$model = new Model_Song();
+//$model->getAllSong();
+?>
---
+++ site/testindex.php
@@ -0,0 +1,6 @@
+
+	<?php
+	include_once('Controller/C_Song.php');
+	$controller = new Ctrl_Song();
+	$controller->invoke();
+	?>
